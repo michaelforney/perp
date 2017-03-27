af7f2e74d2be0a41fbac639f0082a63c6cb6918e
Michael Forney <mforney@mforney.org>
2017-03-26 17:40:10 -0700

    Import perp-2.07
    SHA256:
    1222fe31c16014d8b2a78416f93ba9f8c31eddbc381adc9021fa5d9764475815 perp-2.07.tar.gz

--- conf.mk
+++ conf.mk
@@ -1,6 +1,6 @@
 # conf.mk
 # project build/install configs
-# wcm, 2009.09.14 - 2011.03.22
+# wcm, 2009.09.14 - 2013.01.08
 # ===
 
 ## build configuration, standard gcc + libc:
@@ -11,6 +11,10 @@ CFLAGS = -Wall -Wextra -Wshadow -DNDEBUG -O2
 ## build configuration, dietlibc:
 #CC = diet -Os gcc
 #CFLAGS = -Wall -Wextra -Wshadow -DNDEBUG
+
+## strip configuration
+STRIP = strip
+#STRIP = /some/other/stripper
 
 ## install configuration:
 BINDIR = /usr/bin
